b93dac052a4cc24e5fd7893c93644cd2749f2fbf
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-05-30 21:03:12 +0200

    Add Navbar tests

---
+++ src/components/Navbar/Navbar.test.tsx
@@ -0,0 +1,73 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Navbar from "./Navbar";
+import { ThemeProvider } from "styled-components";
+import theme from "../../styles/theme/theme";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+
+describe("Given a Navbar component", () => {
+  const routes: RouteObject[] = [
+    {
+      path: "/",
+      element: <Navbar />,
+    },
+  ];
+
+  describe("When it renders", () => {
+    test("Then it should show a burger menu button", () => {
+      const appRouter = createMemoryRouter(routes);
+      render(
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={appRouter} />
+        </ThemeProvider>
+      );
+
+      expect(screen.getByRole("button")).toBeInTheDocument();
+    });
+  });
+  describe("When menu button is click", () => {
+    test("Then it should show a className open", () => {
+      const appRouter = createMemoryRouter(routes);
+      const expectedClassName = "open";
+
+      render(
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={appRouter} />
+        </ThemeProvider>
+      );
+
+      const button = screen.getByRole("button");
+
+      fireEvent.click(button);
+
+      const menu = screen.getByRole("list");
+
+      expect(menu).toBeInTheDocument();
+      expect(menu.className).toContain(expectedClassName);
+    });
+    test("Then it should show a link Home, Create and Login", () => {
+      const appRouter = createMemoryRouter(routes);
+
+      render(
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={appRouter} />
+        </ThemeProvider>
+      );
+
+      const button = screen.getByRole("button");
+
+      fireEvent.click(button);
+
+      const homelink = screen.getByRole("link", { name: "Home" });
+      const createlink = screen.getByRole("link", { name: "Create" });
+      const loginlink = screen.getByRole("link", { name: "Login" });
+
+      expect(homelink).toBeInTheDocument();
+      expect(createlink).toBeInTheDocument();
+      expect(loginlink).toBeInTheDocument();
+    });
+  });
+});
