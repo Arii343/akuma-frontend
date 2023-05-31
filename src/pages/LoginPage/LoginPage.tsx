fd75d9915e0a7c03488985072828539cd4d5b8ac
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-05-31 10:02:27 +0200

    Add LoginPage and LoginPageStyled.tsx

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,11 @@
+import LoginPageStyled from "./LoginPageStyled";
+
+const LoginPage = (): React.ReactElement => {
+  return (
+    <LoginPageStyled>
+      <h1 className="login__title">Login</h1>
+    </LoginPageStyled>
+  );
+};
+
+export default LoginPage;
