fb7bc852fc2ea1b8538af535e564aa92df11f9c8
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-01 12:30:25 +0200

    Refactor all tests with renderWithProviders

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,39 +1,19 @@
-import { ThemeProvider } from "styled-components";
 import Header from "./Header";
-import theme from "../../styles/theme/theme";
-import { render, screen } from "@testing-library/react";
-import {
-  RouteObject,
-  RouterProvider,
-  createMemoryRouter,
-} from "react-router-dom";
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../../utils/testUtils";
 
 describe("Given a Header component", () => {
-  const routes: RouteObject[] = [
-    {
-      path: "/",
-      element: <Header />,
-    },
-  ];
-  const router = createMemoryRouter(routes);
   describe("When it renders", () => {
     test("Then it should show a logo 'あKUMA'", () => {
-      render(
-        <ThemeProvider theme={theme}>
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      );
+      renderWithProviders(<Header />);
 
       const logo = screen.getByRole("img", { name: "akuma logo" });
 
       expect(logo).toBeInTheDocument();
     });
+
     test("Then it should show a button menu burger", () => {
-      render(
-        <ThemeProvider theme={theme}>
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      );
+      renderWithProviders(<Header />);
 
       const button = screen.getByRole("button");
 
--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,16 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import LoginPage from "./LoginPage";
-import { ThemeProvider } from "styled-components";
-import theme from "../../styles/theme/theme";
+import renderWithProviders from "../../utils/testUtils";
 
 describe("Given a LoginPage page", () => {
   describe("When it renders", () => {
     test("Then it should show 'Login' title", () => {
-      render(
-        <ThemeProvider theme={theme}>
-          <LoginPage />
-        </ThemeProvider>
-      );
+      renderWithProviders(<LoginPage />);
 
       const expectedTitle = "Login";
 
