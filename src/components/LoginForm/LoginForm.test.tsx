36fd0e9b1e4c3a4c56637bed2f48b938ad124fae
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-01 09:15:58 +0200

    Add LoginForm input test

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import LoginForm from "./LoginForm";
 import { ThemeProvider } from "styled-components";
 import theme from "../../styles/theme/theme";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a LoginForm component", () => {
   describe("When it renders", () => {
@@ -40,6 +41,24 @@ describe("Given a LoginForm component", () => {
 
       const loginButton = screen.getByRole("button", { name: "Login" });
       expect(loginButton).toBeInTheDocument();
+    });
+  });
+  describe("When the user types 'ariadna@gmail.com' in the email input", () => {
+    test("Then it should show that text inside the input", async () => {
+      render(
+        <ThemeProvider theme={theme}>
+          <LoginForm />
+        </ThemeProvider>
+      );
+
+      const inputEmail: HTMLInputElement = screen.getByRole("textbox", {
+        name: "email input",
+      });
+      const expectedText = "ariadna@gmail.com";
+
+      await userEvent.type(inputEmail, expectedText);
+
+      expect(inputEmail.value).toBe(expectedText);
     });
   });
 });
