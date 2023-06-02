e7b2630fc74e25ca89f2e56aa2608a6c9d3bdbf4
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-02 10:00:44 +0200

    Add handleOnSubmit function in LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,16 +1,20 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
+import useUser from "../../hooks/useUser/useUser";
+import { UserCredentials } from "../../store/user/types";
 import LoginPageStyled from "./LoginPageStyled";
 
 const LoginPage = (): React.ReactElement => {
+  const { getUserToken } = useUser();
+
+  const handleOnSubmit = async (userCredentials: UserCredentials) => {
+    await getUserToken(userCredentials);
+  };
+
   return (
     <>
       <LoginPageStyled className="login">
         <h1 className="login__title">Login</h1>
-        <LoginForm
-          handleOnSubmit={() => {
-            ("");
-          }}
-        />
+        <LoginForm handleOnSubmit={handleOnSubmit} />
       </LoginPageStyled>
     </>
   );
