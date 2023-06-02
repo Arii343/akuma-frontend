a09da5f9999be2956d95bf7093800d8faa05b34c
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-02 19:11:43 +0200

    Add userToken test and userDataMock

--- src/mocks/user/userMocks.ts
+++ src/mocks/user/userMocks.ts
@@ -1,4 +1,8 @@
-import { UserStateStructure, UserTokenStructure } from "../../store/user/types";
+import {
+  UserData,
+  UserStateStructure,
+  UserTokenStructure,
+} from "../../store/user/types";
 
 export const emptyUserMock: UserStateStructure = {
   id: "",
@@ -13,3 +17,7 @@ export const userWithTokenMock: UserTokenStructure = {
 
 export const tokenMock =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDc5MDE3YjIxOTVmYzUwM2M1MTMzM2IiLCJpYXQiOjE2ODU2NTE5NDksImV4cCI6MTY4NTczODM0OX0.nZyumE1_KkIVy9eeb5HW9739Vop5cpaF6YSnXsgXJb4";
+
+export const userDataMock: UserData = {
+  id: "6479017b2195fc503c51333b",
+};
