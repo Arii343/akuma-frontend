58b69aaaceff015410374e1f92443b9bec71e01b
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-03 16:20:22 +0200

    Add logoutUser reducer test and add loggedInUserMock

--- src/mocks/user/userMocks.ts
+++ src/mocks/user/userMocks.ts
@@ -16,6 +16,13 @@ export const userWithTokenMock: UserTokenStructure = {
   token: "jdfnjbBBwefffrfvfv",
 };
 
+export const loggedInUserMock: UserStateStructure = {
+  id: "4",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDc5MDE3YjIxOTVmYzUwM2M1MTMzM2IiLCJpYXQiOjE2ODU2NTE5NDksImV4cCI6MTY4NTczODM0OX0.nZyumE1_KkIVy9eeb5HW9739Vop5cpaF6YSnXsgXJb4",
+  isLogged: true,
+};
+
 export const tokenMock =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDc5MDE3YjIxOTVmYzUwM2M1MTMzM2IiLCJpYXQiOjE2ODU2NTE5NDksImV4cCI6MTY4NTczODM0OX0.nZyumE1_KkIVy9eeb5HW9739Vop5cpaF6YSnXsgXJb4";
 
--- src/store/user/userReducer.test.ts
+++ src/store/user/userReducer.test.ts
@@ -1,6 +1,11 @@
+import { emptyUserMock, loggedInUserMock } from "../../mocks/user/userMocks";
 import { userWithTokenMock } from "../../mocks/user/userMocks";
 import { UserStateStructure } from "./types";
-import { loginUserActionCreator, userReducer } from "./userSlice";
+import {
+  loginUserActionCreator,
+  logoutUserActionCreator,
+  userReducer,
+} from "./userSlice";
 
 describe("Given a loginUser reducer", () => {
   describe("When it receives a empty currentState and loginUser action", () => {
@@ -19,6 +24,20 @@ describe("Given a loginUser reducer", () => {
       };
 
       const action = loginUserActionCreator(user);
+      const newState = userReducer(currentState, action);
+
+      expect(newState).toStrictEqual(expectedNewState);
+    });
+  });
+});
+
+describe("Given a logoutUser reducer", () => {
+  describe("When it is called", () => {
+    test("Then it should return a new state with initial state values", () => {
+      const expectedNewState = emptyUserMock;
+      const currentState = loggedInUserMock;
+
+      const action = logoutUserActionCreator();
       const newState = userReducer(currentState, action);
 
       expect(newState).toStrictEqual(expectedNewState);
