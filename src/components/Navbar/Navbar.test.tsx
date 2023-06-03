ce13c7218c147e3530209d9014d4d1c682756b8c
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-03 20:04:33 +0200

    Add Navbar tests when user is logged in

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -2,6 +2,7 @@ import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Navbar from "./Navbar";
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
+import { loggedInUserMock } from "../../mocks/user/userMocks";
 
 describe("Given a Navbar component", () => {
   describe("When it renders", () => {
@@ -12,21 +13,59 @@ describe("Given a Navbar component", () => {
     });
   });
 
-  describe("When menu button is click", () => {
-    test("Then it should show a link Home, Create and Login", async () => {
-      renderWithProviders(wrapWithRouter(<Navbar />));
+  describe("When it renders and the user is not logged in", () => {
+    describe("When menu button is click", () => {
+      test("Then it should show a link Home and Login", async () => {
+        renderWithProviders(wrapWithRouter(<Navbar />));
 
-      const button = screen.getByRole("button");
+        const button = screen.getByRole("button");
 
-      await userEvent.click(button);
+        await userEvent.click(button);
 
-      const homelink = screen.getByRole("link", { name: "Home" });
-      const createlink = screen.getByRole("link", { name: "Create" });
-      const loginlink = screen.getByRole("link", { name: "Login" });
+        const homelink = screen.getByRole("link", { name: "Home" });
+        const loginlink = screen.getByRole("link", { name: "Login" });
+
+        expect(homelink).toBeInTheDocument();
+        expect(loginlink).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe("When it renders and the user is logged in", () => {
+    const preloadedStateUserLoggedIn = {
+      user: loggedInUserMock,
+    };
+    test("Then it should show links home and create and a logout button", () => {
+      renderWithProviders(
+        wrapWithRouter(<Navbar />),
+        preloadedStateUserLoggedIn
+      );
+      const homelink = screen.getByText("Home");
+      const createlink = screen.getByText("Create");
+      const logoutButton = screen.getByText("Logout");
 
       expect(homelink).toBeInTheDocument();
       expect(createlink).toBeInTheDocument();
-      expect(loginlink).toBeInTheDocument();
+      expect(logoutButton).toBeInTheDocument();
+    });
+
+    describe("When the logout button is click", () => {
+      test("Then it should show Login link and hide lougout button", async () => {
+        renderWithProviders(
+          wrapWithRouter(<Navbar />),
+          preloadedStateUserLoggedIn
+        );
+
+        const logoutButton = screen.getByText("Logout");
+
+        await userEvent.click(logoutButton);
+
+        expect(logoutButton).not.toBeInTheDocument();
+
+        const loginlink = screen.getByText("Login");
+
+        expect(loginlink).toBeInTheDocument();
+      });
     });
   });
 });
