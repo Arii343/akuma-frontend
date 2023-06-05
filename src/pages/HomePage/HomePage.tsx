987293cceca925b1ac82c4aceae7f223236c4abe
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-05 15:44:13 +0200

    Add dispatch loadAnimes in HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { useAppDispatch } from "../../store";
 import HomePageStyled from "./HomePageStyled";
+import { loadAnimesActionCreator } from "../../store/animes/animesSlice";
+import { animesMock } from "../../mocks/animes/animesMocks";
 
 const HomePage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadAnimesActionCreator(animesMock));
+  }, [dispatch]);
+
   return (
     <HomePageStyled className="home-page">
       <h1 className="home-page__title">Anime</h1>
