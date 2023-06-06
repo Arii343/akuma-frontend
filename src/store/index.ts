4fdc39ec685a47f8f7e5aa27513d8dae0d06a4c7
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-06 19:14:33 +0200

    Add uiReducer to rootReucer

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,10 +4,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { userReducer } from "./user/userSlice";
 import { animesReducer } from "./animes/animesSlice";
+import { uiReducer } from "./ui/uiSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
   animes: animesReducer,
+  ui: uiReducer,
 });
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
