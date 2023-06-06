72142f28caa2e8af9939563d7617fc267384fc85
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-06 15:54:18 +0200

    Add five more animes to animesMock

--- src/mocks/animes/animesMocks.ts
+++ src/mocks/animes/animesMocks.ts
@@ -1,5 +1,20 @@
 export const animesMock = [
   {
+    id: "1",
+    englishTitle: "D.N.Angel",
+    image: "https://cdn.myanimelist.net/images/anime/1126/98904l.webp",
+  },
+  {
+    id: "2",
+    englishTitle: "D.C.~Da Capo~",
+    image: "https://cdn.myanimelist.net/images/anime/4/73936l.webp",
+  },
+  {
+    id: "3",
+    englishTitle: "Battle Doll Angelic Layer",
+    image: "https://cdn.myanimelist.net/images/anime/1192/113699l.webp",
+  },
+  {
     id: "4",
     englishTitle: "One punch man",
     image: "https://cdn.myanimelist.net/images/anime/1286/99889l.webp",
@@ -23,6 +38,16 @@ export const animesMock = [
     id: "8",
     englishTitle: "Boku no hero",
     image: "https://cdn.myanimelist.net/images/anime/11/11636l.webp",
+  },
+  {
+    id: "9",
+    englishTitle: "Chrono Crusade",
+    image: "https://cdn.myanimelist.net/images/anime/13/14141l.webp",
+  },
+  {
+    id: "10",
+    englishTitle: "Chobits",
+    image: "https://cdn.myanimelist.net/images/anime/4/24648l.webp",
   },
 ];
 
