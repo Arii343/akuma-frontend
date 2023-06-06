8a76e3526ca78d2858f7070d19d7bc02abd1a9db
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-06 19:19:14 +0200

    Use isLoading from ui to toggle AnimeCard or AnimeCardSkeleton

--- src/components/AnimeList/AnimeList.tsx
+++ src/components/AnimeList/AnimeList.tsx
@@ -1,19 +1,25 @@
 import { useAppSelector } from "../../store";
 import AnimeCard from "../AnimeCard /AnimeCard";
+import AnimeCardSkeleton from "../AnimeCardSkeleton/AnimeCardSkeleton";
 import AnimeListStyled from "./AnimeListStyled";
 
 const AnimeList = (): React.ReactElement => {
   const animes = useAppSelector((state) => state.animes.animes);
+  const isLoading = useAppSelector((state) => state.ui.isLoading);
 
   return (
     <AnimeListStyled>
-      {animes.map((anime) => (
-        <AnimeCard
-          imageUrl={anime.image}
-          title={anime.englishTitle}
-          key={anime.id}
-        />
-      ))}
+      {isLoading
+        ? Array.from({ length: 10 }).map((_element, index) => (
+            <AnimeCardSkeleton key={index} />
+          ))
+        : animes.map((anime) => (
+            <AnimeCard
+              imageUrl={anime.image}
+              title={anime.englishTitle}
+              key={anime.id}
+            />
+          ))}
     </AnimeListStyled>
   );
 };
