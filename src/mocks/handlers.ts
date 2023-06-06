f5b4cea36e4af56b976d9f0d17aa9719625e0482
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-06 15:54:57 +0200

    Add anime api route to msw handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,10 +1,14 @@
 import { rest } from "msw";
 import { tokenMock } from "./user/userMocks";
+import { animesMock } from "./animes/animesMocks";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const handlers = [
   rest.post(`${apiUrl}user/login`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ token: tokenMock }));
+  }),
+  rest.get(`${apiUrl}anime`, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ animes: animesMock }));
   }),
 ];
