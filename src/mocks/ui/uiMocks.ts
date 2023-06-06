5723cfdf9c202c898bdec72d01c090101b8759df
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-06 18:06:46 +0200

    Add uiMocks shownLoadingStateMock and hiddenLoadingStateMock

---
+++ src/mocks/ui/uiMocks.ts
@@ -0,0 +1,9 @@
+import { UiStateStructure } from "../../store/ui/types";
+
+export const shownLoadingStateMock: UiStateStructure = {
+  isLoading: true,
+};
+
+export const hiddenLoadingStateMock: UiStateStructure = {
+  isLoading: false,
+};
