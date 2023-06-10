5655b10bd78ab8ed948645b64f2525435719deb6
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-10 02:37:01 +0200

    Add aria-label prop in button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,17 +4,24 @@ interface ButtonProps {
   onClick?: () => void;
   className: string;
   type?: "button" | "reset" | "submit";
+  ariaLabel?: string;
   children: ReactNode;
 }
 
 const Button = ({
   onClick,
-  children,
   className,
   type = "button",
+  ariaLabel = "",
+  children,
 }: ButtonProps): React.ReactElement => {
   return (
-    <button onClick={onClick} className={className} type={type}>
+    <button
+      onClick={onClick}
+      className={className}
+      type={type}
+      aria-label={ariaLabel}
+    >
       {children}
     </button>
   );
