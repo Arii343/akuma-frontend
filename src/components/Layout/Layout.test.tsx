4520df7c7b3b9fafe8e2ac7efc4e82baa27190a7
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-10 17:59:15 +0200

    Add test to check that Loader component renders when showSpinner is true

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -12,4 +12,21 @@ describe("Given a Leyout component", () => {
       expect(header).toBeInTheDocument();
     });
   });
+  describe("When it reders and showSpinner is true", () => {
+    test("Then it should show a Loader component", () => {
+      const preloadedState = {
+        ui: {
+          showSkeleton: false,
+          showSpinner: true,
+        },
+      };
+      renderWithProviders(wrapWithRouter(<Layout />), preloadedState);
+
+      const loader = screen.getByRole("generic", {
+        name: "loader",
+      });
+
+      expect(loader).toBeInTheDocument();
+    });
+  });
 });
