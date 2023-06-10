961105a00c56b911b6e977631a9bdbc918cbdc9b
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-10 17:51:27 +0200

    Render Loader component when showSpinner is true in Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,16 @@
+import { useAppSelector } from "../../store";
 import Header from "../Header/Header";
+import Loader from "../Loader/Loader";
 import LayoutSytled from "./LayoutStyled";
 import { Outlet } from "react-router-dom";
 
 const Layout = (): React.ReactElement => {
+  const showSpinner = useAppSelector((state) => state.ui.showSpinner);
   return (
     <LayoutSytled>
       <Header />
       <Outlet />
+      {showSpinner && <Loader />}
     </LayoutSytled>
   );
 };
