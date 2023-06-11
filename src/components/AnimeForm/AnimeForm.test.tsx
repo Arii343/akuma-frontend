18a671128e383a173f66c400997fe1a1781a15dd
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-12 00:19:09 +0200

    Add tests to check that AnimeForm html elements renders correctly

---
+++ src/components/AnimeForm/AnimeForm.test.tsx
@@ -0,0 +1,197 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../utils/testUtils";
+import AnimeForm from "./AnimeForm";
+
+describe("Given a AnimeForm component", () => {
+  describe("When it renders", () => {
+    test("Then it should show the 'English title' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "English title";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Japanese title' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Japanese title";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Release year' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Release year";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Rating' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Rating";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Demographics' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Demographics";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Genres' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Genres";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Image' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Image";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Score' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Score";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Rank' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Rank";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Popularity' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Popularity";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Type' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Type";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Source' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Source";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Episodes' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Episodes";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Status' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Status";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Duration' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const label = "Duration";
+
+      const expectedInput = screen.getByLabelText(label);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Synopsis' input", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const expectedText = "Write the anime synopsis";
+
+      const expectedInput = screen.getByLabelText(expectedText);
+
+      expect(expectedInput).toBeInTheDocument();
+    });
+
+    test("Then it should show the 'Rates', 'General info' and 'Synopsis' titles", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const ratesTitle = "Rates";
+      const generalInfoTitle = "General info";
+      const synopsisTitle = "Synopsis";
+
+      const expectedRates = screen.getByRole("heading", { name: ratesTitle });
+      const expectedGeneralInfo = screen.getByRole("heading", {
+        name: generalInfoTitle,
+      });
+      const expectedSynopsis = screen.getByRole("heading", {
+        name: synopsisTitle,
+      });
+
+      expect(expectedRates).toBeInTheDocument();
+      expect(expectedGeneralInfo).toBeInTheDocument();
+      expect(expectedSynopsis).toBeInTheDocument();
+    });
+
+    test("Then it should show a button with the 'Create' text", () => {
+      renderWithProviders(<AnimeForm />);
+
+      const expectedButtonText = "Create";
+
+      const button = screen.getByRole("button", { name: expectedButtonText });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
