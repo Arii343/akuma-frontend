d85bc15b73c33fcdae13f4838ec4ff4de2ab5b05
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-11 18:35:14 +0200

    Add CreateAnimePage and styles

---
+++ src/pages/CreateAnimePage/CreateAnimePageStyled.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const CreateAnimePageStyled = styled.div`
+  margin-top: ${(props) => props.theme.spacing.extraExtraLarge};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .create-anime__title {
+    color: ${(props) => props.theme.color.secondary};
+    font-weight: ${(props) => props.theme.font.weight.extraBold};
+    font-size: ${(props) => props.theme.font.size.extraLarge};
+  }
+`;
+
+export default CreateAnimePageStyled;
