1069af59e12f28709d94c04309aae758b9b6cb2e
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-11 18:42:20 +0200

    Add LazyCreateAnimePage in lazyComponents

--- src/routers/lazyComponents/lazyComponents.tsx
+++ src/routers/lazyComponents/lazyComponents.tsx
@@ -3,4 +3,9 @@ import { lazy } from "react";
 export const LazyLoginPage = lazy(
   () => import("../../pages/LoginPage/LoginPage")
 );
+
 export const LazyHomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+
+export const LazyCreateAnimePage = lazy(
+  () => import("../../pages/CreateAnimePage/CreateAnimePage")
+);
