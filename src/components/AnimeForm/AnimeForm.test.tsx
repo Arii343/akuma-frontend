1c7748665dea5e9f73ec8df6aa73faa974709b0b
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-12 03:19:11 +0200

    Add AnimeForm state tests

--- src/components/AnimeForm/AnimeForm.test.tsx
+++ src/components/AnimeForm/AnimeForm.test.tsx
@@ -1,6 +1,7 @@
 import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../utils/testUtils";
 import AnimeForm from "./AnimeForm";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a AnimeForm component", () => {
   describe("When it renders", () => {
@@ -172,6 +173,246 @@ describe("Given a AnimeForm component", () => {
       const button = screen.getByRole("button", { name: expectedButtonText });
 
       expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user types 'My Hero Academia Season 6' in the englishTitle input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputEnglishTitle: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime english title",
+      });
+      const expectedText = "My Hero Academia Season 6";
+
+      await userEvent.type(inputEnglishTitle, expectedText);
+
+      expect(inputEnglishTitle).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types '僕のヒーローアカデミア' in the japaneseTitle input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputJapaneseTitle: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime japanese title",
+      });
+      const expectedText = "僕のヒーローアカデミア";
+
+      await userEvent.type(inputJapaneseTitle, expectedText);
+
+      expect(inputJapaneseTitle).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types '2022' in the releaseYear input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputReleaseYear: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime release year",
+      });
+      const expectedText = "2022";
+
+      await userEvent.type(inputReleaseYear, expectedText);
+
+      expect(inputReleaseYear).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types 'PG-13 - Teens 13 or older' in the rating input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputRating: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime rating",
+      });
+      const expectedText = "PG-13 - Teens 13 or older";
+
+      await userEvent.type(inputRating, expectedText);
+
+      expect(inputRating).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types 'Shounen, Seinen' in the demographics input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputDemographics: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the demographics of the anime",
+      });
+      const expectedText = "Shounen, Seinen";
+
+      await userEvent.type(inputDemographics, expectedText);
+
+      expect(inputDemographics).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types 'Action, Drama' in the genres input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputGenres: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime genres",
+      });
+      const expectedText = "Action, Drama";
+
+      await userEvent.type(inputGenres, expectedText);
+
+      expect(inputGenres).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types 'https://anime-image.png' in the image input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputImage: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Insert anime image url",
+      });
+      const expectedText = "https://anime-image.png";
+
+      await userEvent.type(inputImage, expectedText);
+
+      expect(inputImage).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types '8.37' in the score input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputScore: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime score out of ten",
+      });
+      const expectedText = "8.37";
+
+      await userEvent.type(inputScore, expectedText);
+
+      expect(inputScore).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types '194' in the rank input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputRank: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime rank",
+      });
+      const expectedText = "194";
+
+      await userEvent.type(inputRank, expectedText);
+
+      expect(inputRank).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types '380' in the pupularity input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputPopularity: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime popularity",
+      });
+      const expectedText = "380";
+
+      await userEvent.type(inputPopularity, expectedText);
+
+      expect(inputPopularity).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types 'TV' in the type input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputType: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime type",
+      });
+      const expectedText = "TV";
+
+      await userEvent.type(inputType, expectedText);
+
+      expect(inputType).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types 'Mange' in the source input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputSource: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime source",
+      });
+      const expectedText = "Mange";
+
+      await userEvent.type(inputSource, expectedText);
+
+      expect(inputSource).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types '25' in th episodes input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputEpisodes: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime episodes",
+      });
+      const expectedText = "25";
+
+      await userEvent.type(inputEpisodes, expectedText);
+
+      expect(inputEpisodes).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types 'Finished Airing' in the status input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputStatus: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime status",
+      });
+      const expectedText = "Finished Airing";
+
+      await userEvent.type(inputStatus, expectedText);
+
+      expect(inputStatus).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types '23 min' in the duration input", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputDuration: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime duration",
+      });
+      const expectedText = "23 min";
+
+      await userEvent.type(inputDuration, expectedText);
+
+      expect(inputDuration).toHaveValue(expectedText);
+    });
+  });
+
+  describe("When the user types 'With Tomura Shigaraki at its helm, the former...' in the synopsis textarea", () => {
+    test("Then it should show that text inside the input", async () => {
+      renderWithProviders(<AnimeForm />);
+
+      const inputSynopsis: HTMLInputElement = screen.getByRole("textbox", {
+        name: "Write the anime synopsis",
+      });
+      const expectedText = "With Tomura Shigaraki at its helm, the former...";
+
+      await userEvent.type(inputSynopsis, expectedText);
+
+      expect(inputSynopsis).toHaveValue(expectedText);
     });
   });
 });
