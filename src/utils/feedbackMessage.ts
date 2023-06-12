b69dce981f704a9870c300258a73bbbce2d4e09e
Ariadna Cañizares <ariadnacs@hotmail.com>
2023-06-13 01:40:08 +0200

    Add feedbackMessage object to utils

---
+++ src/utils/feedbackMessage.ts
@@ -0,0 +1,9 @@
+export const feedbackMessage = {
+  successLogin: "You have successfully logged in",
+  successCreateAnime: "Anime created",
+  successDeleteAnime: "Anime deleted",
+  errorLogin: "Wrong credentials",
+  errorLoadAnime: "An error has ocurred while loading anime",
+  errorCreateAnime: "Could not create the anime",
+  errorDeleteAnime: "Could not delete the anime",
+};
